3a55800c3bdc6321338f34c9d069f31383cbc058
Olivier FAURAX <olivier+git@faurax.fr>
2016-02-03 17:50:10 +0100

    Compatibility with python 3.5: print()

--- Tests/xor16_test.py
+++ Tests/xor16_test.py
@@ -1,16 +1,15 @@
 
 from cryptotraces.block_ciphers import xor16
 
-print 'coucou'
 c = xor16.Xor16()
 l = c.get_blocklength()
 lk = c.get_keylength()
 c.load_key(range(1,lk+1))
 c.load_data(range(0,l))
-print c.trace()
+print(c.trace())
 c.encrypt()
-print c.trace()
+print(c.trace())
 c.decrypt()
-print c.trace()
+print(c.trace())
     
 
