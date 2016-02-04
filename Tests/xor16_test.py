5d46cea1f1a5a19028dae53175d7a5f12ad8b048
Olivier FAURAX <olivier+git@faurax.fr>
2016-02-04 09:51:52 +0100

    Cast ranges to lists

--- Tests/xor16_test.py
+++ Tests/xor16_test.py
@@ -4,12 +4,13 @@ from cryptotraces.block_ciphers import xor16
 c = xor16.Xor16()
 l = c.get_blocklength()
 lk = c.get_keylength()
-c.load_key(range(1,lk+1))
-c.load_data(range(0,l))
+c.load_key(list(range(1,lk+1)))
+c.load_data(list(range(0,l)))
 print(c.trace())
 c.encrypt()
 print(c.trace())
 c.decrypt()
 print(c.trace())
     
-
+#fail
+raise AssertionErrors
