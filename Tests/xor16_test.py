d8cfb6b7cc34727ccbc38e7056afa6a454a4b84b
Olivier FAURAX <olivier+git@faurax.fr>
2016-02-04 10:33:35 +0100

    Xor16 test with nose

--- Tests/xor16_test.py
+++ Tests/xor16_test.py
@@ -2,20 +2,25 @@
 import pprint
 from cryptotraces.block_ciphers import xor16
 
-c = xor16.Xor16()
-l = c.get_blocklength()
-lk = c.get_keylength()
-ref = list(range(0,l))
+def xor16_test():
+    c = xor16.Xor16()
+    l = c.get_blocklength()
+    lk = c.get_keylength()
+    ref = list(range(0,l))
+    
+    c.load_key(list(range(1,lk+1)))
+    c.load_data(ref)
+    
+    pp = pprint.PrettyPrinter()
+    pp.pprint(c.trace())
+    c.encrypt()
 
-c.load_key(list(range(1,lk+1)))
-c.load_data(ref)
+    pp.pprint(c.trace())
+    pp.pprint(ref)
+    assert ref != c.trace()['data']
 
-pp = pprint.PrettyPrinter()
-pp.pprint(c.trace())
-c.encrypt()
-pp.pprint(c.trace())
-c.decrypt()
-pp.pprint(c.trace())
-
-#fail -> raise AssertionError
-assert ref == c.trace()['data']
+    c.decrypt()
+    pp.pprint(c.trace())
+    
+    #fail -> raise AssertionError
+    assert ref == c.trace()['data']
